62989c839ee878c1d438d7a671c2f95403032328
Dhanushthulasimani <147314095+Dhanushthulasimani@users.noreply.github.com>
2024-07-23 13:41:17 +0530

    Add files via upload

---
+++ Inordertree.java
@@ -0,0 +1,36 @@
+public class Inordertree {
+    static class Node {
+        int data;
+        Node left;
+        Node right;
+        public Node(int data) {
+            this.data = data;
+            this.left = null;
+            this.right = null;
+        }
+    }
+
+    public static void inorder(Node root) {
+        if (root == null) {
+            return;
+        }
+        inorder(root.left);
+        System.out.print(root.data + " ");
+
+        inorder(root.right);
+
+    }
+
+    public static void main(String[] args) {
+        Node root = new Node(1);
+        root.left = new Node(2);
+        root.right = new Node(3);
+        root.left.left = new Node(4);
+        root.left.right = new Node(5);
+        root.right.right = new Node(6);
+
+
+        System.out.println("Inorder traversal of binary tree is:");
+        inorder(root);
+    }
+}
